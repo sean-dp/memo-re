6c537d3923366a1484cfb0b5e79a91a857a4c0a5
sean-dp <daikiahal902@outlook.jp>
2023-01-11 00:33:50 +0900

    first commit

---
+++ memore/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
